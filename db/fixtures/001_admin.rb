128ca9791d3a24b4136329ceaf01932b5acced68
tm-minhtang <minh.tang@tomosia.com>
2020-03-13 15:58:41 +0700

    fix seed_fu

--- db/fixtures/001_admin.rb
+++
@@ -1,2 +0,0 @@
-User.delete_all
-User.create! email: 'admin@tomosia.com', password: 'Abc123@@@', is_admin: true
--- db/fixtures/002_guest.rb
+++ db/fixtures/002_guest.rb
@@ -1,4 +1,5 @@
 User.delete_all
+User.create! email: 'admin@tomosia.com', password: 'Abc123@@@', is_admin: true
 5.times do |n|
   User.create! email: "guest-#{n}@tomosia.com", password: 'Abc123@@@', is_admin: false
 end
